071d887ecada3ba8442c787745e914c498b80e24
anc0 <ak6688@student.uni-lj.si>
2018-01-09 21:52:02 +0000

    Local startup added.

--- src/main/java/com/zgodnji/fifastatstracker/LeaderboardResource.java
+++ src/main/java/com/zgodnji/fifastatstracker/LeaderboardResource.java
@@ -1,11 +1,25 @@
 package com.zgodnji.fifastatstracker;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
 import java.util.List;
+
 
 @RequestScoped
 @Path("leaderboards")
@@ -71,5 +85,81 @@ public class LeaderboardResource {
                 properties.getIntegerProperty());
 
         return Response.ok(response).build();
+    }
+
+
+    /*@Inject
+    @DiscoverService(value = "scores-service", environment = "dev", version = "1.0.0")
+    private URL url;
+
+    @Inject
+    @DiscoverService(value = "scores-service", environment = "dev", version = "1.0.0")
+    private WebTarget webTarget;*/
+
+
+    @Inject
+    @DiscoverService(value = "scores-service", environment = "dev", version = "1.0.0")
+    private String scoresUrlString;
+
+    @GET
+    @Path("{gameId}/leaderboard")
+    public Response viewLeaderboardForGame() {
+
+        System.out.println("=====================");
+
+        StringBuilder response = new StringBuilder();
+
+        response.append("{\"list\":");
+
+        try {
+            URL url = new URL(scoresUrlString + "/v1/scores");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output;
+            while ((output = br.readLine()) != null) {
+                response.append(output);
+            }
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.append("}");
+
+        System.out.println(response.toString());
+        try {
+
+            Object obj = JSONValue.parse(response.toString());
+
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println("-------------");
+            System.out.println(jsonObject);
+
+            /*String name = (String) jsonObject.get("name");
+            System.out.println(name);*/
+
+            System.out.println("--------------");
+            // loop array
+            JSONArray scores = (JSONArray) jsonObject.get("list");
+            System.out.println(scores);
+
+            for (int i = 0; i < scores.size(); i++) {
+                System.out.println("++++++++++++");
+                System.out.println(i);
+                System.out.println(scores.get(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=====================");
+        return Response.noContent().build();
     }
 }
